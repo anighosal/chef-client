b19603418ea33a86650a54d6f5d319eb9ebf880c
anighosal <121676026+anighosal@users.noreply.github.com>
2023-05-02 00:59:32 +0600

    banner added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import banner from "../../../assets/fotor_2023-5-2_0_52_49.jpg";
 
 const Header = () => {
   return (
     <div>
-      <h2>this is header</h2>
+      <div className="ms-5">
+        <img fluid className="mt-5 pt-5" src={banner} alt="" />
+      </div>
     </div>
   );
 };
